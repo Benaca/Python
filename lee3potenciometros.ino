805976a1faa6f84d6588509b4fe60448be7e2969
Antonio de la Rosa <38260606+Benaca@users.noreply.github.com>
2018-04-27 21:40:47 +0200

    Lee el valor de 3 potenciómetros
    Lee el valor de 3 potenciómetros y envía la información por el puerto Serie

---
+++ lee3potenciometros.ino
@@ -0,0 +1,20 @@
+const int pinRED = A0;
+const int pinGREEN = A1;
+const int pinBLUE = A2;
+
+
+void setup(){
+  Serial.begin(115200); 
+}
+
+void loop(){ 
+
+  Serial.print("R:");
+  Serial.println(analogRead(A0));
+  Serial.print("G:");
+  Serial.println(analogRead(A1));
+  Serial.print("B:");
+  Serial.println(analogRead(A2));
+  delay(100);
+  
+}
